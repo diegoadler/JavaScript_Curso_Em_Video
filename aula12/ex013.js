33acc186f4ccc0305ea10a2c5a3dd339281ffa28
Diego Assunção <35967898+diegoadler@users.noreply.github.com>
2022-07-15 12:08:53 -0300

    update

---
+++ aula12/ex013.js
@@ -0,0 +1,8 @@
+var agora = new Date()
+var diaHora = agora.getHours()
+var diaMinuto = agora.getMinutes()
+var semana = agora.getDate()
+
+console.log(diaHora)
+console.log(diaMinuto)
+console.log(semana)
